3e0cec6c839493741c2d34f2db8deca15da3415d
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2023-12-16 22:54:56 -0500

    somechanges

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # watchme
  Let someone watch you?
 
+# Pre requisites
+- Python3
+    - Depending on your OS install a global python 3 and its related pip version. 
+- waitress cli
+    - this should be install on a global level.
+    - for Ubuntu i have used
+    - 
+        ```bash
+        sudo apt install python3-waitress
+        ```
+    
+- flask
+    - This can be installed with pip
+    - ```bash
+        pip install falsk
+        ```
+- opencv for camera
+    - For getting camera feed
+    - ```bash
+        pip install opencv-python
+        ```
+    - ```bash
+        pip install opencv-contrib-python
+        ```
+- obvious a webcam
+
--- app/app.py
+++ app/app.py
@@ -3,16 +3,17 @@ import cv2
 
 app = Flask(__name__)
 camera_indices = [0,1] 
-cameras = [cv2.VideoCapture(index) for index in camera_indices]
 
-def generate_frames():
-    while True:
-        frames = [cap.read()[1] for cap in cameras]
-        # frames = [cv2.resize(frame, (400, 300)) for frame in frames]  # Optional: Resize frames for consistent display
-        ret, buffer = cv2.imencode('.jpg', cv2.hconcat(frames))
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+# def generate_frames():
+#     cameras = [cv2.VideoCapture(index) for index in camera_indices]
+    
+#     while True:
+#         frames = [cap.read()[1] for cap in cameras]
+#         # frames = [cv2.resize(frame, (400, 300)) for frame in frames]  # Optional: Resize frames for consistent display
+#         ret, buffer = cv2.imencode('.jpg', cv2.hconcat(frames))
+#         frame = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 def generate_frames_me():
     camera = cv2.VideoCapture(0) 
     while True:
@@ -24,7 +25,9 @@ def generate_frames_me():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames_me(), mimetype='multipart/x-mixed-replace; boundary=frame')    
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -32,14 +35,12 @@ def index():
 @app.route('/me')
 def Show_me():
     return render_template('index.html')
-@app.route('/all')
-def Show_all():
-    return render_template('allCams.html')
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames_me(), mimetype='multipart/x-mixed-replace; boundary=frame')
-@app.route('/all_video')
-def video_feed_all():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/all')
+# def Show_all():
+#     return render_template('allCams.html')
+
+# @app.route('/all_video')
+# def video_feed_all():
+#     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == '__main__':
     app.run(debug=True)
